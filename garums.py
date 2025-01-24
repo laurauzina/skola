e69ba091c07f38552d9d7c5244724fbb6e84f087
laurauzina <laurauzina@gmail.com>
2025-01-24 07:42:10 +0000

    v03

---
+++ garums.py
@@ -0,0 +1,10 @@
+import math
+print("ievadiet koordinātas: ")
+xa = int(input())
+xb = int(input())
+ya = int(input())
+yb = int(input())
+
+garums = math.sqrt((xa-xb)**2+(ya-yb)**2)
+
+print (garums)
---
+++ taisnlenka_trijsturis.py
@@ -0,0 +1,16 @@
+import math
+print("ievadiet koordinātas: ")
+xa = int(input())
+xb = int(input())
+ya = int(input())
+yb = int(input())
+za = int(input())
+zb = int(input())
+
+hip_gar =((xa-xb)**2+(ya-yb)**2)
+kat1_gar =((xa-zb)**2+(xb-zb)**2)
+kat2_gar =((xb-za)**2+(xb-zb)**2)
+
+if(hip_gar)==(kat1_gar)+(kat2_gar) and 
+
+print (garums)
